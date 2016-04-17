fb5bf3f260fc1734d7c9f6fa0797d07f543deaf5
kaburke <kaburke@clemson.edu>
2016-04-17 15:57:34 -0400

    created function to process and write to txt file

--- pid_processor.py
+++ pid_processor.py
@@ -1,21 +1,31 @@
+#WRITTEN WITH PYTHON 3.5.1
+#by: Kyle Burkett 4.17.16
+
 #using time to sleep to fake processing
 import time
-
+# Function to process pid
+def process( str ):
+    print ("Processing patient with id: " + pid + "... ", end="")
+    time.sleep(1)
+    with open('current_pid.txt', 'w') as f:
+        f.write(pid)
+        f.close()
+    print ("complete.")
+    return
 #get starting point
 with open('current_pid.txt', 'r') as f:
     startnum = f.read()
+    print ("Start processing patient with id: " + startnum)
     startnum = int(startnum)
+    f.close()
 #read and sort pids in int array
 with open('pids.txt', 'r') as f:
     pid_raw = f.read().splitlines()
 pid_ints = [int(i) for i in pid_raw]
 pid_ints.sort()
 #cycle through ints to find starting point
-for num in pid_ints:
-    if num == startnum:
-        for num in pid_ints:
-            print ("Processing patient with id: " + str(num) + "... ", end="")
-            time.sleep(1)
-            print ("complete.")
+for num in (y for y in pid_ints if y > startnum):
+    pid = str(num)
+    process(pid)
 
-#print (pid_ints)
+print ("All Patients In pids.txt Now Processed")
